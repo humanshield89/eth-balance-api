15beb210a7e30836c82b7a5dd735171b17c959dc
Rachid Boudjelida <rachidboudjelida@gmail.com>
2023-12-05 16:34:41 +0100

    add a catch all route, log the right port

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", balanceRouter);
+app.use("*", (req, res) => {
+  res.status(404).send("The are not the routes you are looking for");
+});
 
 app.listen(process.env.PORT, () => {
-  console.log("Server started on port 3000");
+  console.log(`Server started on port ${process.env.PORT}`);
 });
